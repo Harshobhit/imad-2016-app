12e55fcf22fd0279c11d5f5b735202a1f38c9f11
Harshobhit <dayalharshobhit@gmail.com>
2016-12-27 11:16:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article', function (req, res) {
-  res.send("Hello");
+  res.send('Hello');
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
