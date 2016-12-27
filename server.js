93e530b488e6e1ba5c73435254b10b830f74a7d1
Harshobhit <dayalharshobhit@gmail.com>
2016-12-27 13:50:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+  user: 'harshobhit',
+   database: 'harshobhit',
+   host:'harshobhit.imad.hasura-app.io',
+   port:'5432',
+  password: 'db-harshobhit-11999',
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,10 +16,23 @@ var counter=0;
 app.get('/counter',function(req,res)
 {
    counter=counter+1;
-   res.send(counter.toString())
+   res.send(counter.toString());
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/userlogin', function (req, res) {
+  pool.query('SELECT * FROM user',function(err,result){
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.stringify(result));
+      }
+});
 });
 
 app.get('/ui/style.css', function (req, res) {
