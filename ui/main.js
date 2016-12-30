2720fb63fbb2d1f5d26cf5aabd7561b365cb290d
Harshobhit <dayalharshobhit@gmail.com>
2016-12-31 00:01:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,23 +9,14 @@ var register = document.getElementById('register');
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                var stat=`<div class="container">
-    <h2>Alerts</h2>
-    <div class="alert alert-success">
-      <strong>Success!</strong>successfully registered to our website
-    </div>
-    </div>`;
-              }
-        }
-
         // Make the request
         var username = document.getElementById('username').value;
-        var emailid=document.getElementById('emailid').value
+        var emailid=document.getElementById('emailid').value;
         var password = document.getElementById('password').value;
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, emailid:emailid,password: password}));
-        register.value = 'Registering...';
-
+        request.send(JSON.stringify({username: username,emailid:emailid,password: password}));
+              }
+          }              
     };
 };
