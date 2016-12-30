de6afe6724e0ff8506369b3d36dd9d4ff3e9d959
Harshobhit <dayalharshobhit@gmail.com>
2016-12-31 00:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 var register = document.getElementById('register');
     register.onclick = function () {
+        alert("hi");
         // Create a request object
         var request = new XMLHttpRequest();
 
@@ -13,7 +14,7 @@ var register = document.getElementById('register');
         var username = document.getElementById('username').value;
         var emailid=document.getElementById('emailid').value;
         var password = document.getElementById('password').value;
-        request.open('GET', '/create-user', true);
+        request.open('GET', '/createuser', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username,emailid: emailid,password: password}));
         
